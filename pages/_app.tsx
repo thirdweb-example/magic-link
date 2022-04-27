d0184710adf9dab8965ef0319a9a40f92c27f5ee
Jarrod Watts <jarrodwatts16@gmail.com>
2022-04-27 18:11:15 +1000

    initial code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,35 @@
-import type { AppProps } from 'next/app';
-import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+import "./styles/globals.css";
+import type { AppProps } from "next/app";
+import {
+  ChainId,
+  ThirdwebProvider,
+  WalletConnector,
+} from "@thirdweb-dev/react";
 
 // This is the chainId your dApp will work on.
-const activeChainId = ChainId.Mainnet;
+const activeChainId = ChainId.Mumbai;
+
+// Our Magic Link Wallet Connector configuration
+const magicLinkWalletConnector: WalletConnector = {
+  name: "magic",
+  options: {
+    // Replace this with your own magic link api key
+    apiKey: "pk_live_05XXXXXXXXXXXXXX",
+    rpcUrls: {
+      [ChainId.Mumbai]: "https://mumbai.magic.io/rpc",
+    },
+  },
+};
+
+// Array of wallet connectors you want to use for your dApp.
+const connectors = [magicLinkWalletConnector];
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider desiredChainId={activeChainId}>
+    <ThirdwebProvider
+      desiredChainId={activeChainId}
+      walletConnectors={connectors}
+    >
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,40 @@
-import { useAddress, useDisconnect, useMetamask } from '@thirdweb-dev/react';
-import type { NextPage } from 'next';
+import styles from "./styles/Home.module.css";
+import { useAddress, useDisconnect, useMagic } from "@thirdweb-dev/react";
+import type { NextPage } from "next";
+import { useState } from "react";
 
 const Home: NextPage = () => {
-  const address = useAddress();
-  const connectWithMetamask = useMetamask();
-  const disconnectWallet = useDisconnect();
+  const address = useAddress(); // Hook to grab the currently connected user's address.
+  const connectWithMagic = useMagic(); // Hook to connect with Magic Link.
+  const disconnectWallet = useDisconnect(); // Hook to disconnect from the connected wallet.
+  const [email, setEmail] = useState<string>(""); // State to hold the email address the user entered.
+
   return (
-    <div>
+    <div className={styles.container}>
+      {/* If there is a wallet connected, show disconnect and the user's address */}
       {address ? (
         <>
-          <button onClick={disconnectWallet}>Disconnect Wallet</button>
+          <button className={styles.btn} onClick={disconnectWallet}>
+            Disconnect Wallet
+          </button>
           <p>Your address: {address}</p>
         </>
       ) : (
-        <button onClick={connectWithMetamask}>Connect with Metamask</button>
+        // If there is no wallet connected, show the email input and the Magic Link connect button.
+        <>
+          <input
+            className={styles.input}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <button
+            className={styles.btn}
+            onClick={() => connectWithMagic({ email })}
+          >
+            Login
+          </button>
+        </>
       )}
     </div>
   );
